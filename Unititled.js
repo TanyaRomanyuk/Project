5b9cc88e751ebc1e69fa5fb1e7803f3e2cca9122
TanyaRomanyuk <44803599+TanyaRomanyuk@users.noreply.github.com>
2018-11-12 22:47:05 +0200

    Update Unititled.js

--- Unititled.js
+++ Unititled.js
@@ -13,40 +13,114 @@ arr.forEach(function(element){
 console.log(element);
 })
     
-var students = [
-{   firstName: 'Tanya', lastName: 'Romanyuk', ratting: '90'},
-{   firstName: 'Angela', lastName: 'Catavasia', ratting: '65'},
-{   firstName: 'Olga', lastName: 'Lipaka',   ratting: '75'},
-{   firstName: 'Igor', lastName: 'Mandzyuk', ratting: '95'}
-];
- students.sort(function(a, b){
- if (a.firstName > b.firstName){
-     return 1;
- }
- if (a.firstName < b.firstName){
-    return -1;
-}
-    return 0;
-})
-console.log(students);
-
-students.sort(function(c, d){
-    if (c.ratting > d.ratting){
-        return 1;
-    }
-    if (c.ratting < d.ratting){
-       return -1;
-   }
-       return 0;
-   })
-   console.log(students);
+let students = [
+    {
+        firstName: 'Ivan',
+        lastName:  'Aleksienko',
+        ratting:   '75'
+    },
+    {
+        firstName: 'Nazar',
+        lastName:  'Bochnak',
+        ratting: '90'
+    },
    
+    {
+        firstName: 'Nazar',
+        lastName:  'Koshitsky',
+        ratting: '90',
+    },
 
-  function rand ()
-  {
-     var res  = Math.floor(Math.random() * 999999999);
-      return res;
-  }
-  console.log(rand());
+    {
+        firstName: 'Tetyana',
+        lastName:  'Romanyuk',
+        ratting: '90',
+    },
+
+    {
+        firstName: 'Tetyana',
+        lastName:  'Palianytsa',
+        ratting: '92',
+    },
+    {
+        firstName: 'Igor',
+        lastName:  'Mandzyuk',
+        ratting: '89',
+    },
+    {
+        firstName: 'Ivan',
+        lastName:  'Melnichuk',
+        ratting: '78',
+    },
+    {
+        firstName: 'Petro',
+        lastName:  'Podminohin',
+        ratting: '76',
+    },
+    {
+        firstName: 'Oleg',
+        lastName:  'Batun',
+        ratting: '60',
+    },
+    {
+        firstName: 'Bohdan',
+        lastName:  'Timchishin',
+        ratting: '79',
+    },
+]
+
+const sortObj = (arr, prop) => {
+    let compare = (a, b) => a[prop] > b[prop] ? 1 : a[prop] == b[prop] ? 0 : -1; //Сортування.
+    arr.sort(compare);
+}
+
+
+    const compare = (arr, key, callback) => arr.reduce((prev, curr) => 
+    (callback(prev[key], curr[key]) ? prev : curr), {})[key];
+
+    const rateMin = compare(students, 'ratting', (a, b) => a < b); 
+    const rateMax = compare(students, 'ratting', (a, b) => a > b); 
+
+    
+    function ah () { //
+    let sum=0;
+    for (const key in students){
+        sum += +students[key].ratting;
+         
+       };
+    let avg = Math.ceil(sum/students.length);
+    return avg.toString();
+    }
+
+
+
+            function rate (){ 
+                for (const key in students) {
+                 let value;
+                  value = Math.round(((rateMax/students[key].ratting)*100)-100);
+                  students[key].rate = value; 
+                    }
+                }
+
+sortObj(students, 'firstName'); 
+ console.log(students); 
+ console.log('\n')
+ sortObj(students, 'lastName'); 
+ console.log(students);
+ console.log('\n')
+ sortObj(students, 'ratting');
+ console.log(students);
+ 
+ console.log('\n');
+
+
+ console.log('Максимальне, мінімальне та середне значення'+'\n'+`Min value: ${rateMin}, max value: ${rateMax}`); 
+ console.log(students.find(x => x.ratting === ah()));     //Ah value
+ console.log(students.find(x => x.ratting === rateMin));   //Min ratting
+ console.log(students.find(x => x.ratting === rateMax));   //Max ratting
+            
+            rate();
+    console.log('\n'+'Додавання поля rate: ')
+   console.log(students);
 
 
